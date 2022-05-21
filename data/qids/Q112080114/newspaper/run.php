5945ed9a6bbf435dfb065fbe96e264031998bb7f
Jones <michaeljones401@gmail.com>
2022-05-21 15:52:35 +0100

    adding new people

---
+++ data/qids/Q112080114/newspaper/run.php
@@ -0,0 +1,27 @@
+<?php
+
+class Parse {
+
+  public function __construct()
+  {
+
+  }
+
+  private function loopJson()
+  {
+
+  }
+
+  private function parseFullNewspaper($json)
+  {
+
+  }
+
+  private function parseSmallerNewspaper($json)
+  {
+
+  }
+
+}
+
+(new Parse());
